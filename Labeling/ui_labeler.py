5b5d87260bd36bd2f451e74f8f977d182c954ffc
MrGeislinger <9027783+MrGeislinger@users.noreply.github.com>
2023-03-15 22:51:10 -0700

    Write manual labels to file

--- Labeling/ui_labeler.py
+++ Labeling/ui_labeler.py
@@ -5,8 +5,9 @@ import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-from visualize import viz_hand, animation_plot, animation_and_image
+from visualize import viz_hand, animation_and_image
 from semisupervision import *
 
 st.set_page_config(layout="wide")
@@ -44,6 +45,14 @@ label_index = read_dict(f'../data/sign_to_prediction_index_map.json')
 index_label = {label_index[key]: key for key in label_index}
 X_rhand, y = load_data('rhand')
 
+# Button to perform KMeans
+N_CLUSTERS = st.number_input(
+    label='Number of clusters',
+    min_value=5,
+    max_value=100,
+    value=20,
+    step=5,
+)
 
 # Display Signs
 SIGN_NAME = st.selectbox(
@@ -63,17 +72,14 @@ for i,label in enumerate(y):
     y_all_frames[i*N_FRAMES:(i+1)*N_FRAMES] = label
 
 
-# Button to perform KMeans
-N_CLUSTERS = 20
-
 @st.cache_data
-def show_rep_images(X, subset_mask):
+def show_rep_images(X, subset_mask, n_clusters):
     X_subset = X[subset_mask]
     X_subset = X_subset.reshape(
         X_subset.shape[0]*X_subset.shape[1],
         X_subset.shape[-1]
     )
-    kmeans = cluster_frames(X_subset, k=N_CLUSTERS)
+    kmeans = cluster_frames(X_subset, k=n_clusters)
     
     rep_frames, rep_frame_idx = get_representative_images(
         X,
@@ -84,11 +90,11 @@ def show_rep_images(X, subset_mask):
 
     base_size = 3
     fig = plt.figure(
-        figsize=(5*base_size, (N_CLUSTERS//5)*base_size),
+        figsize=(5*base_size, (n_clusters//5)*base_size),
     )
 
-    for j in range(N_CLUSTERS):
-        ax = fig.add_subplot(N_CLUSTERS//5, 5, j+1)
+    for j in range(n_clusters):
+        ax = fig.add_subplot(n_clusters//5, 5, j+1)
         f_rhand = rep_frames[j].reshape(-1,2).copy()
         viz_hand(
             ax=ax,
@@ -98,45 +104,65 @@ def show_rep_images(X, subset_mask):
     st.pyplot(fig)
     return rep_frame_idx
 
-frame_index = show_rep_images(X_rhand, subset_mask=mask)
+frame_index = show_rep_images(X_rhand, subset_mask=mask, n_clusters=N_CLUSTERS)
 selection_containers = {}
-choice = {}
+# choice = {}
 
 
-# @st.cache_data()
-def display_choice(frame_data, frame_idx, sign_name, key_id, _containers):
+@st.cache_data()
+def display_choice(_frame_data, frame_idx, sign_name, _col,):
     animation = animation_and_image(
-        frame_data,
+        _frame_data,
         main_frame_idx=frame_idx,
         sign_name=sign_name,
     )
-    col1, col2, col3 = st.columns(3)
-    
-    with col1:
-        st.components.v1.html(animation.to_html5_video(), width=300, height=400,)
+    with _col:
+        st.components.v1.html(
+            animation.to_html5_video(),
+            width=300,
+            height=400,
+        )
 
-    # Select frame name
-    _containers[key_id] = temp = col2.container()
 
-    # Show examples of frames?
-    col3.write('temp3')
+results_container = st.container()
 
-with st.form('my_form'):
-    instance_idx = 0
-    submitted = st.form_submit_button("Submit")
-    temp = st.container()
+state = st.session_state
+def write_labels_to_file():
+    df = pd.DataFrame(
+        data=[
+            (
+                f_idx,
+                f_idx // 15,
+                f_idx % 15,
+                state[f_idx],
+            )
+            for f_idx in frame_index
+        ],
+        columns=('frame_id', 'video', 'relative_frame', 'handshape'),
+    )
+    results_container.write(df)
+    # Write data to file
+    fname = datetime.strftime(datetime.now(), '%Y%m%d-%H%M%S')
+    df.to_csv(f'label-{fname}.csv', index=False)
+    # TODO: Confirm?
+
+form = st.form('my_form', clear_on_submit=True)
+with form:
+    submitted = st.form_submit_button(
+        'Save results',
+        on_click=write_labels_to_file
+    )
     for i,frame_idx in enumerate(frame_index):
+        col1, col2, col3 = form.columns(3)
         display_choice(
-            frame_data=X_rhand[frame_idx//15].reshape(-1,21,2),
+            _frame_data=X_rhand[frame_idx//15].reshape(-1,21,2),
             frame_idx=frame_idx%15,
             sign_name=index_label[y_all_frames[frame_idx]],
-            key_id=i,
-            _containers=selection_containers,
+            _col=col1,
         )
-        st.write(len(selection_containers))
-        # Every form must have a submit button.
-        with selection_containers[i]:
-            choice[i] = st.selectbox('handshape', handshapes['gloss'], key=i)
-    if submitted:
-       temp.write('submitted')
-       temp.write(choice)
+        col2.selectbox('handshape', handshapes['gloss'], key=frame_idx)
+        col3.write(f'{frame_idx=}')
+
+if submitted:
+    st.write('submitted')
+    
